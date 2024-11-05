1a08d9b8a25b0090690a1f4ea622dc50dbde475b
Dups aure <duplonhannah08@gmail.com>
2024-11-05 16:41:59 +0800

    event_page_function

--- src/Event/EventList.jsx
+++ src/Event/EventList.jsx
@@ -1,72 +1,35 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
+import { Link } from "react-router-dom";
+import axios from "axios";
 import ReactPaginate from "react-paginate";
 import "./EventList.css";
 import bloodimage from "../assets/images/blood.jpg";
 
-const events = [
-  {
-    id: 1,
-    title: "Blood Donation Drive",
-    description:
-      "A blood donation occurs when a person voluntarily has blood drawn and used for transfusions and/or made into biopharmaceutical medications by a process called fractionation (separation of whole blood components). A donation may be of whole blood or of specific components directly (apheresis). Blood banks often participate in the collection process as well as the procedures that follow it.",
-    date: "October 1 - October 13, 2024",
-    place: "National University Dasmarinas, 4th Floor",
-    image: bloodimage,
-  },
-  {
-    id: 2,
-    title: "Health Awareness Seminar",
-    description:
-      "A blood donation occurs when a person voluntarily has blood drawn and used for transfusions and/or made into biopharmaceutical medications by a process called fractionation (separation of whole blood components). A donation may be of whole blood or of specific components directly (apheresis). Blood banks often participate in the collection process as well as the procedures that follow it.",
-    date: "October 14, 2024",
-    place: "National University Dasmarinas, Auditorium",
-    image: bloodimage,
-  },
-  {
-    id: 3,
-    title: "Charity Fun Run",
-    description:
-      "A blood donation occurs when a person voluntarily has blood drawn and used for transfusions and/or made into biopharmaceutical medications by a process called fractionation (separation of whole blood components). A donation may be of whole blood or of specific components directly (apheresis). Blood banks often participate in the collection process as well as the procedures that follow it.",
-    date: "October 20, 2024",
-    place: "National University Dasmarinas, Sports Complex",
-    image: bloodimage,
-  },
-  {
-    id: 4,
-    title: "Volunteer Day",
-    description:
-      "A blood donation occurs when a person voluntarily has blood drawn and used for transfusions and/or made into biopharmaceutical medications by a process called fractionation (separation of whole blood components). A donation may be of whole blood or of specific components directly (apheresis). Blood banks often participate in the collection process as well as the procedures that follow it.",
-    date: "October 27, 2024",
-    place: "National University Dasmarinas, Community Center",
-    image: bloodimage,
-  },
-  {
-    id: 5,
-    title: "Food Drive",
-    description:
-      "A blood donation occurs when a person voluntarily has blood drawn and used for transfusions and/or made into biopharmaceutical medications by a process called fractionation (separation of whole blood components). A donation may be of whole blood or of specific components directly (apheresis). Blood banks often participate in the collection process as well as the procedures that follow it.",
-    date: "November 3, 2024",
-    place: "National University Dasmarinas, Main Hall",
-    image: bloodimage,
-  },
-  {
-    id: 6,
-    title: "Workshop on Mental Health",
-    description:
-      "A blood donation occurs when a person voluntarily has blood drawn and used for transfusions and/or made into biopharmaceutical medications by a process called fractionation (separation of whole blood components). A donation may be of whole blood or of specific components directly (apheresis). Blood banks often participate in the collection process as well as the procedures that follow it.",
-    date: "November 10, 2024",
-    place: "National University Dasmarinas, Room 201",
-    image: bloodimage,
-  },
-];
+export default function EventList() {
+  const [eventList, setEventList] = useState([]);
 
-const EventList = () => {
-  const itemsPerPage = 5;
+  useEffect(() => {
+    axios
+      .get(
+        "http://localhost/agap-backend-main/api/phase_1/read/readEvents.php",
+        {
+          headers: {
+            "Content-Type": "application/json",
+          },
+        }
+      )
+      .then(function (response) {
+        console.log("this is the event list ", response.data.data);
+        setEventList(response.data.data);
+      });
+  }, []);
+
   const [currentPage, setCurrentPage] = useState(0);
+  const itemsPerPage = 5;
 
-  const pageCount = Math.ceil(events.length / itemsPerPage);
+  const pageCount = Math.ceil(eventList.length / itemsPerPage);
 
-  const currentEvents = events.slice(
+  const currentEvents = eventList.slice(
     currentPage * itemsPerPage,
     (currentPage + 1) * itemsPerPage
   );
@@ -75,48 +38,91 @@ const EventList = () => {
     setCurrentPage(data.selected);
   };
 
+  const [showFilter, setShowFilter] = useState(false);
+
+  const toggleFilter = () => {
+    setShowFilter(!showFilter);
+  };
+
+  const handleFilterOption = (option) => {
+    console.log(`Filter selected: ${option}`);
+    setShowFilter(false);
+  };
+
   return (
-    <div className="event-list-container">
-      {currentEvents.map((event) => (
-        <div key={event.id} className="event-container">
-          <img src={event.image} alt={event.title} className="event-image" />
-          <div className="event-content">
-            <h2 className="headerEventList">{event.title}</h2>
-            <p className="description">{event.description}</p>
-            <p className="date">📅 {event.date}</p>
-            <p className="location">📍 {event.place}</p>
-            <hr className="divider" />
-            <div className="action-buttons">
-              <button className="donate-button">Donate</button>
-              <button className="volunteer-button">Volunteer</button>
+    <>
+      <div className="eSearch">
+        <h1 className="eList">Event List</h1>
+
+        <div className="bSearch">
+          <input
+            type="text"
+            placeholder="Search events..."
+            className="searchInput"
+          />
+        </div>
+
+        <div className="eFilter">
+          <button className="filterButton" onClick={toggleFilter}>
+            Filter Options
+          </button>
+          {showFilter && (
+            <div className="filterOptions">
+              <div onClick={() => handleFilterOption("Oldest to Newest")}>
+                Oldest to Newest
+              </div>
+              <div onClick={() => handleFilterOption("Newest to Oldest")}>
+                Newest to Oldest
+              </div>
+            </div>
+          )}
+        </div>
+      </div>
+
+      <div className="event-list-container">
+        {currentEvents.map((event, key) => (
+          <div key={key} className="event-container">
+            <img src={bloodimage} className="event-image" />
+            <div className="event-content">
+              <h2 className="headerEventList">{event.event_name}</h2>
+              <p className="description">{event.description}</p>
+              <p className="date">📅 {event.start_date}</p>
+              <p className="event_link">📍 {event.event_link}</p>
+              <hr className="divider" />
+              <div className="action-buttons">
+                <Link className="donate-button" to={"/Donate"}>
+                  Donate
+                </Link>
+                <Link className="volunteer-button" to={"/VolunteerDashboard"}>
+                  Volunteer
+                </Link>
+              </div>
             </div>
           </div>
+        ))}
+
+        <div className="pagination-container">
+          <ReactPaginate
+            previousLabel={<span>&laquo;</span>}
+            nextLabel={<span>&raquo;</span>}
+            breakLabel={"..."}
+            pageCount={pageCount}
+            marginPagesDisplayed={2}
+            pageRangeDisplayed={5}
+            onPageChange={handlePageClick}
+            containerClassName={"pagination"}
+            pageClassName={"pagination-item"}
+            pageLinkClassName={"pagination-link"}
+            previousClassName={"pagination-item"}
+            previousLinkClassName={"pagination-link"}
+            nextClassName={"pagination-item"}
+            nextLinkClassName={"pagination-link"}
+            breakClassName={"pagination-item"}
+            breakLinkClassName={"pagination-link"}
+            activeClassName={"active"}
+          />
         </div>
-      ))}
-
-      <div className="pagination-container">
-        <ReactPaginate
-          previousLabel={<span>&laquo;</span>}
-          nextLabel={<span>&raquo;</span>}
-          breakLabel={"..."}
-          pageCount={pageCount}
-          marginPagesDisplayed={2}
-          pageRangeDisplayed={5}
-          onPageChange={handlePageClick}
-          containerClassName={"pagination"}
-          pageClassName={"pagination-item"}
-          pageLinkClassName={"pagination-link"}
-          previousClassName={"pagination-item"}
-          previousLinkClassName={"pagination-link"}
-          nextClassName={"pagination-item"}
-          nextLinkClassName={"pagination-link"}
-          breakClassName={"pagination-item"}
-          breakLinkClassName={"pagination-link"}
-          activeClassName={"active"}
-        />
       </div>
-    </div>
+    </>
   );
-};
-
-export default EventList;
+}
--- src/Event/EventPage.jsx
+++ src/Event/EventPage.jsx
@@ -1,54 +1,19 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 import "./EventPage.css";
 import Navbar from "../Page/NavBar.jsx";
 import EventCar from "./EventCar.jsx";
-import EventList from "./EventList.jsx"
+import EventList from "./EventList.jsx";
 import Footer from "../Page/AgapFooter.jsx";
 
 function EventPage() {
-  const [showFilter, setShowFilter] = useState(false);
-
-  const toggleFilter = () => {
-    setShowFilter(!showFilter);
-  };
-
-  const handleFilterOption = (option) => {
-    console.log(`Filter selected: ${option}`);
-    setShowFilter(false);
-  };
-
   return (
     <div>
       <Navbar />
-      
-      <h1 className='eventtitle'>
-        Events
-      </h1>
 
-      <div className='eCar'>
+      <h1 className="eventtitle">Events</h1>
+
+      <div className="eCar">
         <EventCar />
-      </div>
-
-      <div className='eSearch'>
-        <h1 className='eList'>
-          Event List
-        </h1>
-
-        <div className='bSearch'>
-          <input type="text" placeholder="Search events..." className="searchInput" />
-        </div>
-
-        <div className='eFilter'>
-          <button className="filterButton" onClick={toggleFilter}>
-            Filter Options
-          </button>
-          {showFilter && (
-            <div className="filterOptions">
-              <div onClick={() => handleFilterOption("Oldest to Newest")}>Oldest to Newest</div>
-              <div onClick={() => handleFilterOption("Newest to Oldest")}>Newest to Oldest</div>
-            </div>
-          )}
-        </div>
       </div>
       <EventList />
       <Footer />
